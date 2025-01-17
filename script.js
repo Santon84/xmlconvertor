2865638dafc997a218fb18a78ad84b5d6dfd8290
Anton <santon84@gmail.com>
2025-01-17 15:33:15 +0300

    first

---
+++ script.js
@@ -0,0 +1,75 @@
+
+const categories = { "24" : "Автобетоносмеситель",
+"33" :"Полуприцеп",
+"33?" :"Прицеп",
+"22" :"Самосвал",
+"30" :"Мультилифт",
+"20" : "Тягач",
+"23" :"Бортовой",
+"21" :"Шасси",
+"26" :"Бортовой с КМУ",
+"25" :"Автотопливозаправщик",
+"336" :"Ломовоз"}
+
+
+
+
+async function loadXML() { 
+    try { 
+        
+        // const response = await fetch('./src/test.xml'); 
+        // const xmlString = await response.text(); 
+        
+        const parser = new DOMParser(); 
+        const inXML = document.getElementById('input');
+        const outXML = document.getElementById('output');
+        // console.log(inXML.value);
+        const xmlDoc = parser.parseFromString(inXML.value, 'text/xml'); 
+
+        let newXML = `<?xml version="1.0"?>
+                      <data>
+                      <cars>
+                      `;
+
+        const cars = xmlDoc.getElementsByTagName("offer");
+
+        for (let i = 0; i < cars.length; i++) {
+          // const name = cars[i].getElementsByTagName("model")[0].textContent;
+          const price = cars[i].getElementsByTagName("price")[0].textContent;
+          const vendor = cars[i].getElementsByTagName("vendor")[0].textContent;
+          const model = cars[i].getElementsByTagName("model")[0].textContent;
+          const url = cars[i].getElementsByTagName("url")[0].textContent;
+          const picture = cars[i].getElementsByTagName("picture")[0].textContent;
+          const categoryId = cars[i].getElementsByTagName("categoryId")[0].textContent;
+          const color = 'Белый';
+
+          newXML+= `<car>
+          <unique_id>${i}</unique_id>
+          <mark_id>${vendor}</mark_id>
+          <folder_id>${model}</folder_id>
+          <url>${url}</url>
+          <images>${picture}</images>
+          <body_type>${categories[categoryId]}</body_type>
+          <color>${color}</color>
+          <availability>В наличии</availability>
+          <custom>Растаможен</custom>
+          <year>2024</year>
+          <price>${price}</price>
+          <currency>RUR</currency>
+          <owners_number>Не было владельцев</owners_number>
+          </car>
+          `
+        
+        } 
+        newXML+= `</cars>
+                  </data> `;
+        outXML.value = newXML;
+
+    } catch (error) { 
+        console.error('Error loading XML:', error); 
+    } 
+    
+}
+
+
+
